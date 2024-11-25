6e4392f1424737460cf691f4b6f5d0f03bc47de2
infra-02 <infra-02@webdrone.fr>
2024-11-25 19:18:13 +0100

    create a common createCronJob to manage error and have common function

--- cron/cron_entrypoint.sh
+++ cron/cron_entrypoint.sh
@@ -26,6 +26,10 @@ LOG_DIR=${LOG_DIR:-/var/log}
 SCRIPT_NAME="${SCRIPT_NAME:-cron}.log"
 LOG_FILE=${LOG_DIR}/${SCRIPT_NAME}
 
+## cron parameters
+CRON_BASE_DIR=${CRON_BASE_DIR:-/etc/cron.d/}
+
+
 ####################################################
 #                    Dependencies
 ####################################################
@@ -38,13 +42,28 @@ LOG_FILE=${LOG_DIR}/${SCRIPT_NAME}
 
 checkMandatoryVariable() {
 ### valid that all variables tagged as mandatory are defined ###
-    log "checking mandatory variables existence"
     for var in "${MANDATORY_VAR_LIST[@]}"; do
         if [[ -z "${var+x}" ]]; then
             error "$var is not defined or is empty."
             return 1
         fi
     done
+}
+
+createCronJob() {
+### create cron jon
+###    arg 1 : cron schedule
+###    arg 2 : cron job action
+###    arg 3 : cron job name
+###
+    $TEMP_CRON_SCHEDULE=$(echo "$1" | tr -d '"')
+    $TEMP_CRON_JOB=$(echo "$2" | tr -d '"')
+    CRON_CREATION_OC=$(echo "${TEMP_CRON_SCHEDULE} ${TEMP_CRON_JOB}  &> /var/log/cron.log" >> ${CRON_BASE_DIR}/$3)
+    if [ $? -ne 0 ]; then
+        error_exit "error while creating now cron job : ${CRON_CREATION_OC}"
+    else
+
+    fi
 }
 
 ####################################################
@@ -56,7 +75,7 @@ if [ $? -ne 0 ]; then
     error_exit "mandatory variables above not set, see previous logs to see which, exiting"
 fi
 
-echo "${CRON_SCHEDULE} ${CRON_JOB}  &> /var/log/cron.log" >> /etc/cron.d/backup_job
+createCronJob "${CRON_SCHEDULE}" "${CRON_JOB}" backup_job
 
 ## Create logrotate configuration file
 cat << EOF > /etc/logrotate.d/cron_logs
@@ -74,8 +93,7 @@ cat << EOF > /etc/logrotate.d/cron_logs
 }
 EOF
 
-## Add logrotate scheduled task to crontab
-echo "0 * * * * /usr/sbin/logrotate /etc/logrotate.d/cron_logs" >> /etc/cron.d/logrotate_job
+createCronJob "0 * * * *" "/usr/sbin/logrotate /etc/logrotate.d/cron_logs" logrotate_job
 
 ## Start cron and keep it running in the foreground, while outputting logs
 cron
