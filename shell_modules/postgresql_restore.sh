7f59e943b3f0003539e2ddd189828c2ae0847418
dreadper <dreadper@gmail.com>
2024-12-24 11:07:31 +0100

    try to fix ERROR:  cannot drop the currently open database

--- shell_modules/postgresql_restore.sh
+++ shell_modules/postgresql_restore.sh
@@ -102,19 +102,35 @@ postgresql_restore() {
             return 1
             break
         fi
+        log "postgresql (${DB}) getting dump file process ended (in success) in $(($PG_TERMINATE_CONN_ELAPSED_TIME/60)) min $(($PG_TERMINATE_CONN_ELAPSED_TIME%60)) sec"
+
+        ## Get ride of existing database connection
+        PG_TERMINATE_CONN_DB_START_TIME=$(date +%s)
+        log "postgresql connection termination process started on host : ${POSTGRES_HOST} for Database : ${DB}"
+        PG_TERMINATE_CONN_RESULT=$(psql -h ${POSTGRES_HOST} -U ${POSTGRES_USERNAME} -d ${DB} -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = ${DB} AND pid <> pg_backend_pid();")
+        if [ $? -ne 0 ]; then
+            PG_TERMINATE_CONN_ELAPSED_TIME=$(( $(date +%s)-${PG_TERMINATE_CONN_DB_START_TIME} ))
+            error "postgresql (${DB}) connection termination process ended (in error) in $(($PG_TERMINATE_CONN_ELAPSED_TIME/60)) min $(($PG_TERMINATE_CONN_ELAPSED_TIME%60)) sec"
+            error $PG_TERMINATE_CONN_RESULT
+            PG_RESTORE_SUCCESS=false
+            return 1
+            break
+        fi
+        log "postgresql (${DB}) connection termination process ended (in success) in $(($PG_TERMINATE_CONN_ELAPSED_TIME/60)) min $(($PG_TERMINATE_CONN_ELAPSED_TIME%60)) sec"
 
         ## restore database from restic restore
         PG_RESTORE_DB_START_TIME=$(date +%s)
-        log "postgresql restore process started on host : ${POSTGRES_HOST} for Database : ${DB}"
+        log "postgresql database restore process started on host : ${POSTGRES_HOST} for Database : ${DB}"
         PG_RESTORE_RESULT=$(pg_restore -h ${POSTGRES_HOST} -U ${POSTGRES_USERNAME} -d ${DB} -j ${RESTORE_PARALELL_THREAD} --clean --if-exists --create --exit-on-error --format=custom $BACKUP_DMP_FILENAME)
         if [ $? -ne 0 ]; then
             PG_RESTORE_ELAPSED_TIME=$(( $(date +%s)-${PG_RESTORE_DB_START_TIME} ))
-            error "postgresql database restore process ended (in error) in $(($PG_RESTORE_ELAPSED_TIME/60)) min $(($PG_RESTORE_ELAPSED_TIME%60)) sec"
+            error "postgresql database (${DB}) restore process ended (in error) in $(($PG_RESTORE_ELAPSED_TIME/60)) min $(($PG_RESTORE_ELAPSED_TIME%60)) sec"
             error $PG_RESTORE_RESULT
             PG_RESTORE_SUCCESS=false
             return 1
             break
         fi
+        log "postgresql database ${DB} restore process ended (in success) in $(($PG_TERMINATE_CONN_ELAPSED_TIME/60)) min $(($PG_TERMINATE_CONN_ELAPSED_TIME%60)) sec"
     done
 
     PG_RESTORE_SUCCESS=true
